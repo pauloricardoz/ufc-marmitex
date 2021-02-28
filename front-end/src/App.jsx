6629ae2f0fbf8995567a5b651c0c767a3120ebb0
pauloricardoz <trybe.przt@gmail.com>
2021-02-27 22:55:16 -0300

    home page + css

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,23 @@
-import "./App.css";
+import './App.css';
 import LoginPage from './pages/login-page';
-import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './pages/home-page';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="main">
+      <header className="header">
+       {/*  <h1>UFC-Marmitex</h1> */}
+      </header>
       <BrowserRouter>
-        <Route path='/' component={LoginPage} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route path="/" component={LoginPage} />
+        </Switch>
       </BrowserRouter>
+      <footer className="footer">
+        <small>&copy; Copyright 2021, UFC-Marmitex dev-team</small>
+      </footer>
     </div>
   );
 }
--- front-end/src/pages/login-page.jsx
+++ front-end/src/pages/login-page.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function LoginPage() {
+  const history = useHistory();
   return (
     <div className="login-page">
       <p>Quem é você? (:</p>
       <input type="text" />
       <p>Seu PIN (:</p>
       <input type="password" />
-      <button type="button">ENTRAR</button>
+      <button type="button" onClick={() => history.push('/home')}>
+        ENTRAR
+      </button>
     </div>
   );
 }
